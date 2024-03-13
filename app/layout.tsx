44d3d2f22c7d593c35e3a9fbdd0af07fff17c086
Jimi <jimi.denysiak@icloud.com>
2024-03-13 18:27:02 +0100

    karl_soutien INIT

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,45 @@
 import type { Metadata } from "next";
+import localFont from 'next/font/local';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const NeueMontreal = localFont({
+  src: [
+    {
+      path: './NeueMontreal-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './NeueMontreal-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './NeueMontreal-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  preload: false,
+  variable: '--neue',
+  fallback: [
+    'apple-system',
+    'blinkmacsystemfont',
+    'segoe ui',
+    'roboto',
+    'oxygen',
+    'ubuntu',
+    'cantarell',
+    'fira sans',
+    'droid sans',
+    'helvetica neue',
+    'sans-serif',
+  ],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
